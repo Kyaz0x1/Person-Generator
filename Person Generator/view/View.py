d82cf65ab43076814021a5dbfdb93a54262651c8
kyaz0x1 <87987490+kyaz0x1@users.noreply.github.com>
2021-08-07 00:30:31 -0300

    Update View.py

--- Person Generator/view/View.py
+++ Person Generator/view/View.py
@@ -9,7 +9,7 @@ class View:
             print(f'\t{Fore.YELLOW}[{option + 1}] {options[option]}')
     
     @staticmethod
-    def show_menu(title, **options):
+    def show_menu_coptions(title, **options):
         print(f'\t\t{Back.MAGENTA} {title} {Back.RESET}')
         for key, value in options.items():
             print(f'\t{Fore.YELLOW}[{key}] {value}')
